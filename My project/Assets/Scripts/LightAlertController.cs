81f2b9816d15434ed3aaeee0357c6d52dc944b14
maradw <maria.mendoza@tecsup.edu.pe>
2025-02-24 11:47:59 -0500

    35

--- My project/Assets/Scripts/LightAlertController.cs
+++ My project/Assets/Scripts/LightAlertController.cs
@@ -5,21 +5,30 @@ using UnityEngine;
 
 public class LightAlertController : MonoBehaviour
 {
-    Renderer alarmRenderer;
+    Renderer _alarmRenderer;
+
+    Color _baseColor;
+    float _baseTransparency;
+    float _renderT;
     void Start()
-    {
-         alarmRenderer = GetComponent<Renderer>();
+    { 
+         _alarmRenderer = GetComponent<Renderer>();
+        _baseColor = _alarmRenderer.material.color;
+        _baseTransparency = _alarmRenderer.material.color.a;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         
     }
     void LightsOn()
     {
-        alarmRenderer.material.SetColor("Color", Color.green);
-        Debug.Log("q paso causa");
+
+        // _alarmRenderer.material.SetColor("_Color", Color.green);
+        //_alarmRenderer.material.SetColor("_Color_base", Color.green);
+        //parpadeo de luces
+        StartCoroutine(FadeIn());
     }
     void OnEnable()
     {
@@ -28,6 +37,51 @@ public class LightAlertController : MonoBehaviour
     void OnDisable()
     {
         ControlPanel.OnSimulatorOnOff -= LightsOn;
+    }
+    public void CallFadeOut()
+    {
+        StartCoroutine(FadeIn());
+
+    }
+    IEnumerator FadeIn()
+    {
+
+        Color startColor = _baseColor; 
+        Color endColor = Color.red;    
+
+        
+        float duration = 2f;
+
+        float startTransparency = 0;
+        float endTransparency = 1;
+
+        // Comienza el fade
+        for (float color = 0f; color <= 1f; color += 0.15f)
+        {
+            _baseColor = Color.Lerp(startColor, endColor, color);
+            _baseTransparency = Mathf.Lerp(startTransparency, endTransparency, color);
+           // _alarmRenderer.material.SetFloat("_Transparency",1) ;
+            _alarmRenderer.material.color = _baseColor;
+            _alarmRenderer.material.SetFloat("_Transparency", _baseTransparency);
+
+            yield return new WaitForSeconds(duration * 0.15f); 
+        }
+        
+
+        _alarmRenderer.material.color = endColor;
+
+
+
+
+        /*//currentColor = fadeImage.color;
+        for (float color = 1f; color >= -1; color = color - 0.15f)
+        {
+            _baseColor = Color.green;
+            _baseColor = Color.red; 
+            //currentColor.a = color;
+            //fadeImage.color = currentColor;
+            yield return new WaitForSeconds(0.2f);
+        }*/
     }
 
 }
